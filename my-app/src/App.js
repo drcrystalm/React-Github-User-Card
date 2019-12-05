e4c42fa02e449908114888c396673aaee30244b2
Crystal Martin <drcrystalm@gmail.com>
2019-12-05 16:59:41 -0600

    user info renders followers info does not yet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,14 @@ import logo from "./logo.svg"
 import "./App.css"
 import { METHODS } from "http"
 import axios from "axios"
+import Users from "./Components/Users"
 
 class App extends React.Component {
-    state = {
-        user: [],
+    constructor() {
+        super()
+        this.state = {
+            user: [],
+        }
     }
 
     componentDidMount() {
@@ -14,19 +18,50 @@ class App extends React.Component {
             .get(`https://api.github.com/users/drcrystalm`)
 
             .then(res => {
+                // console.log(res)
                 this.setState({
-                    user: res.data.data,
+                    name: res.data.name,
+                    user: res.data.login,
+                    bio: res.data.bio,
+                    company: res.data.company,
+                    location: res.data.location,
+                    followers: res.data.followers,
                 })
             })
             .catch(err => console.log(err, "This is no good"))
     }
 
-    componentDidUpdate
+    // componentDidUpdate() {
+    //     axios
+    //         .get(`https://api.github.com/users/drcrystalm/followers`)
+
+    //         .then(res => {
+    //             console.log(res)
+    //             this.setState({
+    //                 followerName: res.data.login,
+    //             })
+    //         })
+
+    //         .catch(err => console.log(err, "Error from componentDidUpdate"))
+    // }
 
     render() {
+        console.log(this.state)
         return (
             <div>
-                <h1>User Card</h1>
+                <h1>User Card of {this.state.name}</h1>
+                <h3>"{this.state.bio}"</h3>
+                <p>
+                    {this.state.name} lives in {this.state.location} .{" "}
+                    {this.state.name} works for {this.state.company} , check out
+                    her work on GitHub under {this.state.user} .
+                </p>
+                <ul>
+                    <li>
+                        {this.state.user} has {this.state.followers} followers.
+                    </li>
+                </ul>
+                <Users name={this.state.followerName} />
             </div>
         )
     }
--- my-app/src/Components/Users.js
+++ my-app/src/Components/Users.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 function Users(props) {
-    return <div></div>
+    return (
+        <div>
+            <h3>{props.followerName}</h3>
+        </div>
+    )
 }
 
 export default Users
